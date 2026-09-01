afee6884482501394abe647ee5939b59067fbaff
MrSreta <matijasreta@gmail.com>
2026-09-01 19:12:51 +0200

    Delete input.sh

--- input.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Enter the server name:"
-read SERVER_NAME
-
-echo "Enter the environment (prod/staging/dev):"
-read ENVIRONMENT
-
-echo "You entered: $SERVER_NAME in $ENVIRONMENT environment"
