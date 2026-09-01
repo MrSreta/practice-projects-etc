f762e577fa8915dcf9e952d96430cbef77e2fcb0
MrSreta <matijasreta@gmail.com>
2026-09-01 19:12:59 +0200

    Delete myfirst.sh

--- myfirst.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# ^ shebang, this is a comment
-echo "Hello, this is my first script"
-echo "Hello today's day is: $(date)"
-echo "I am logged in as: $(whoami)"
-echo "My current directory is: $(pwd)"
